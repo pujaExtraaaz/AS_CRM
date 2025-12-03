121048597e71369655e90fca41f1d9f622b6e807
pujaExtraaaz <puja.extraaaz@outlook.com>
2025-12-03 16:00:17 +0530

    commited by puja on date 03-12-2025

--- database/migrations/2025_11_27_123923_create_dispositions_table.php
+++ database/migrations/2025_11_27_123923_create_dispositions_table.php
@@ -4,26 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
+
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('dispositions', function (Blueprint $table) {
-            $table->id();
-            $table->string('name')->nullable();
-            $table->integer('created_by')->default(0);
-            $table->timestamps();
-        });
+    public function up(): void {
+        if (!Schema::hasTable('part_type')) {
+            Schema::create('dispositions', function (Blueprint $table) {
+                $table->id();
+                $table->string('name')->nullable();
+                $table->integer('created_by')->default(0);
+                $table->timestamps();
+            });
+        }
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('dispositions');
     }
 };
--- database/migrations/2025_12_01_115416_create_sales_dispute_table.php
+++ database/migrations/2025_12_01_115416_create_sales_dispute_table.php
@@ -10,18 +10,20 @@ return new class extends Migration {
      * Run the migrations.
      */
     public function up(): void {
-        Schema::create('sales_dispute', function (Blueprint $table) {
-            $table->id();
-            $table->integer('user_id')->nullable()->default(0);
-            $table->integer('salesorder_id')->nullable()->default(0);
-            $table->date('dispute_date');
-            $table->decimal('disputed_amount', 30, 2)->default('0.00');
-            $table->decimal('gp_deduction', 30, 2)->default('0.00');
-            $table->decimal('loss', 30, 2)->default('0.00');
-            $table->decimal('total_deduction', 30, 2)->default('0.00');
-            $table->integer('created_by')->default(0);
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('sales_dispute')) {
+            Schema::create('sales_dispute', function (Blueprint $table) {
+                $table->id();
+                $table->integer('user_id')->nullable()->default(0);
+                $table->integer('salesorder_id')->nullable()->default(0);
+                $table->date('dispute_date');
+                $table->decimal('disputed_amount', 30, 2)->default('0.00');
+                $table->decimal('gp_deduction', 30, 2)->default('0.00');
+                $table->decimal('loss', 30, 2)->default('0.00');
+                $table->decimal('total_deduction', 30, 2)->default('0.00');
+                $table->integer('created_by')->default(0);
+                $table->timestamps();
+            });
+        }
     }
 
     /**
